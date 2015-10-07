22e855725ed550fcffde3a57f87627eb542bf090
oneseta <booksomoro@gmail.com>
2015-10-08 07:02:59 +0900

    [AndroidShare]だいぶ酷い問題修正

--- AndroidShare.js
+++ AndroidShare.js
@@ -1,6 +1,8 @@
 var ClipData=android.content.ClipData;
 var ClipDescription=android.content.ClipDescription;
 
+var getAct=com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+
 AndroidShare={
 	clipBoard:function(t){
 		var item=new ClipData.Item(t);
@@ -8,5 +10,7 @@ AndroidShare={
 		mimeType[0]=ClipDescription.MIMETYPE_TEXT_PLAIN;
 		var cd=new ClipData(new ClipDescription("text_data", mimeType),item);
 		print("クリップボードにコピーしました");
+		var cm=getAct.getSystemService(CLIPBOARD_SERVICE);
+		cm.setPrimaryClip(cd);
 	}
 };
