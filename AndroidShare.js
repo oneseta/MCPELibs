de2c3a4eddd79f8ea12d3a47a28cbf11add8c2f6
oneseta <booksomoro@gmail.com>
2015-10-07 23:52:39 +0900

    AndroidShare.js

--- AndroidShare.js
+++ AndroidShare.js
@@ -1,1 +1,11 @@
+var ClipData=android.content.ClipData;
+var ClipDescription=android.content.ClipDescription;
 
+AndroidShare={
+	clipBoard:function(t){
+		var item=new ClipData.Item(t);
+		var mimeType=new java.lang.String[1];
+		mimeType[0]=ClipDescription.MIMETYPE_TEXT_PLAIN;
+		var cd=new ClipData(new ClipDescription("text_data", mimeType),item);
+	}
+};
