b56aaa75e041c7cecd2fe4e0c00d5078b743c635
oneseta <booksomoro@gmail.com>
2015-10-08 16:13:29 +0900

    [AndroidShare]エラー直しなう

--- AndroidShare.js
+++ AndroidShare.js
@@ -6,7 +6,7 @@ getAct=com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
 AndroidShare={
 	clipBoard:function(t){
 		var item=new ClipData.Item(t);
-		var mimeType=new java.lang.String[1];
+		var mimeType=[];
 		mimeType[0]=ClipDescription.MIMETYPE_TEXT_PLAIN;
 		var cd=new ClipData(new ClipDescription("text_data", mimeType),item);
 		print("クリップボードにコピーしました");
